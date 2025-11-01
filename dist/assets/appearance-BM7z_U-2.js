fa6fe066b5248c542e5dccac85163a58c36305de
Zin Min Myat <zinminmyat051@gmail.com>
2025-11-01 23:30:27 +0700

    modified

---
+++ dist/assets/appearance-BM7z_U-2.js
@@ -0,0 +1,16 @@
+import{c as x,u as b,j as s,H as v}from"./app-XgFoaY_8.js";import{a as u,c as h}from"./app-logo-icon-BOPC6mkE.js";import{e as j,S as _,H as N}from"./layout-Di5Z62jA.js";import{A}from"./app-layout-D--9aP4f.js";/* empty css            */import"./index-C8AUngf1.js";import"./index-C4J_hXM0.js";import"./index-DmMlQonR.js";import"./index-BbVhNUHx.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]],S=u("Monitor",M);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],H=u("Moon",w);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const L=[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]],$=u("Sun",L);function q(a){const e=x.c(13);let t,o;e[0]!==a?({className:o,...t}=a,e[0]=a,e[1]=t,e[2]=o):(t=e[1],o=e[2]);const i=o===void 0?"":o,{appearance:p,updateAppearance:m}=b();let l;e[3]===Symbol.for("react.memo_cache_sentinel")?(l=[{value:"light",icon:$,label:"Light"},{value:"dark",icon:H,label:"Dark"},{value:"system",icon:S,label:"System"}],e[3]=l):l=e[3];const y=l;let n;e[4]!==i?(n=h("inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800",i),e[4]=i,e[5]=n):n=e[5];let r;e[6]!==p||e[7]!==m?(r=y.map(k=>{const{value:d,icon:f,label:g}=k;return s.jsxs("button",{onClick:()=>m(d),className:h("flex items-center rounded-md px-3.5 py-1.5 transition-colors",p===d?"bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100":"text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60"),children:[s.jsx(f,{className:"-ml-1 h-4 w-4"}),s.jsx("span",{className:"ml-1.5 text-sm",children:g})]},d)}),e[6]=p,e[7]=m,e[8]=r):r=e[8];let c;return e[9]!==t||e[10]!==n||e[11]!==r?(c=s.jsx("div",{className:n,...t,children:r}),e[9]=t,e[10]=n,e[11]=r,e[12]=c):c=e[12],c}const z=[{title:"Appearance settings",href:j().url}];function F(){const a=x.c(2);let e;a[0]===Symbol.for("react.memo_cache_sentinel")?(e=s.jsx(v,{title:"Appearance settings"}),a[0]=e):e=a[0];let t;return a[1]===Symbol.for("react.memo_cache_sentinel")?(t=s.jsxs(A,{breadcrumbs:z,children:[e,s.jsx(_,{children:s.jsxs("div",{className:"space-y-6",children:[s.jsx(N,{title:"Appearance settings",description:"Update your account's appearance settings"}),s.jsx(q,{})]})})]}),a[1]=t):t=a[1],t}export{F as default};
